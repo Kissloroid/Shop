1aef1191bdab86b5f10155eb85bcea8419b4cf9c
kissl <kissloroid@gmail.com>
2025-11-04 20:00:13 +0300

    Video 3 - class based views

--- main/views.py
+++ main/views.py
@@ -1,16 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
 
-def index(request):
-    context = {
-        'title': 'Kissloroid',
-        'content': 'Главная страница - HOME',
-    }
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
 
-    return render(request, 'main/index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Kissloroid'
+        context['content'] = 'Магазин мебели'
+
+        return context
+
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Kissloroid-about'
+        context['content'] = 'О нас'
+        context['text_on_page'] = 'Всяка ересь'
+        return context
+
+
+# def index(request):
+#     context = {
+#         'title': 'Kissloroid',
+#         'content': 'Главная страница - HOME',
+#     }
+#
+#     return render(request, 'main/index.html', context)
 
 
 def about(request):
