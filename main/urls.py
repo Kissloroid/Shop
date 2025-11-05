be528209884a5b68c4937211484c515f2f6820b1
kissl <kissloroid@gmail.com>
2025-11-05 20:47:01 +0300

    video 4 - caching

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+# from django.views.decorators.cache import cache_page
 from main import views
 
 app_name = 'main'
@@ -9,3 +9,8 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
 
 ]
+
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('about/', cache_page(60)(views.AboutView.as_view()), name='about'),
+# ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,15 @@
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.cache import cache
 from django.contrib.auth.views import LoginView
 from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView, UpdateView, TemplateView
-
 from carts.models import Cart
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
@@ -75,7 +76,7 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(LoginRequiredMixin, UpdateView):
+class UserProfileView(LoginRequiredMixin, CacheMixin, UpdateView):
     template_name = 'users/profile.html'
     form_class = ProfileForm
     success_url = reverse_lazy('users:profile')
@@ -94,12 +95,14 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Кабинет'
-        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
+        orders = Order.objects.filter(user=self.request.user).prefetch_related(
             Prefetch(
                 "orderitem_set",
                 queryset=OrderItem.objects.select_related("product"),
             )
         ).order_by("-id")
+
+        context['orders'] = self.set_get_cache(orders, f"user_{self.request.user.id}_orders", 60)
         return context
 
 
